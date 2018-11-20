df97c0f47412a92c7f4a369e1286aeed2770ca83
lv.karachevtsev <zebra-sherlok-holms@yandex.ru>
2018-11-20 23:47:55 +0300

    server init

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var logger = require('intel');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = 3000;
+
+server.listen(port, '127.0.0.1', function () {
+    var address = server.address();
+    logger.info('Server starting on ' + address.address + ':' + address.port);
+});
